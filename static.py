9d9baf3dc31be0f8917f61a172646dc0d4c0df21
LonnieCoffman <lonniecoffman@icloud.com>
2018-08-15 07:59:01 -0700

    Initial Commit

---
+++ static.py
@@ -0,0 +1,11 @@
+# Path to folder where you will be writing data files from MT4 (eg. ) "C:\\Trading\\MT4\\mql4\\Files\\FXtrade\\"
+filepath = ""
+
+# Oanda account ID
+account_id = ""
+
+# API token
+token = ""
+
+# True for live trading / False for demo trading
+live_trading = True
